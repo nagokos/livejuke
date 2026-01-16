62353f713dd5c5c4d36c4cfd4b6e251be7a61d61
nagokos <kosnago0428@gmail.com>
2026-01-16 09:28:49 +0900

    Add Nix flake dev shell

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "LiveJuke monorepo dev shells (Rust API + Expo RN)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
+        };
+
+        rustToolchain = with pkgs.rust-bin; [
+          (stable.latest.minimal.override {
+            extensions = [
+              "clippy"
+              "rust-src"
+            ];
+          })
+
+          nightly.latest.rustfmt
+          nightly.latest.rust-analyzer
+        ];
+
+        common = with pkgs; [
+          git
+          curl
+          jq
+        ];
+
+        apiPkgs =
+          (with pkgs; [
+            pkg-config
+            postgresql
+          ])
+          ++ rustToolchain;
+
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = common ++ apiPkgs;
+        };
+      }
+    );
+}
