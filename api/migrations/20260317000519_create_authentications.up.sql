49756a81c16a13ffb9ae443d4bb2591ab48322b0
nagokos <kosnago0428@gmail.com>
2026-03-20 12:42:25 +0900

    feat(api): add database migrations for users and authentications

---
+++ api/migrations/20260317000519_create_authentications.up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE authentications(
+	id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+	provider VARCHAR NOT NULL CHECK (provider IN ('email', 'google')), 
+	uid VARCHAR NOT NULL,
+	password_digest VARCHAR,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+	UNIQUE(provider, uid)
+);
+CREATE TRIGGER set_updated_at
+    BEFORE UPDATE ON authentications
+    FOR EACH ROW EXECUTE FUNCTION update_updated_at();
