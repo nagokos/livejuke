e8836f07398618e13cba16ff604875ea872f80b5
nagokos <kosnago0428@gmail.com>
2026-03-20 12:42:25 +0900

    feat(api): wire up email registration in main

--- api/src/config.rs
+++ api/src/config.rs
@@ -1,15 +1,38 @@
-use std::env;
+use std::{env, str::FromStr};
 
 pub struct Config {
+    pub app_env: AppEnv,
     pub database_url: String,
+    pub jwt_secret: String,
+    pub jwt_expiration: i64,
 }
 
 impl Config {
-    pub fn from_env() -> anyhow::Result<Config> {
+    pub fn from_env() -> Result<Config, anyhow::Error> {
         dotenvy::dotenv().ok();
 
         Ok(Config {
+            app_env: env::var("APP_ENV")?.parse()?,
             database_url: env::var("DATABASE_URL")?,
+            jwt_secret: env::var("JWT_SECRET")?,
+            jwt_expiration: env::var("JWT_EXPIRATION")?.parse()?,
         })
+    }
+}
+
+pub enum AppEnv {
+    Development,
+    Production,
+}
+
+impl FromStr for AppEnv {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "development" => Ok(AppEnv::Development),
+            "production" => Ok(AppEnv::Production),
+            _ => Err(anyhow::anyhow!("unknown app env {}", s)),
+        }
     }
 }
--- api/src/main.rs
+++ api/src/main.rs
@@ -1,26 +1,47 @@
-use std::sync::Arc;
+use std::{
+    net::{Ipv4Addr, SocketAddr},
+    sync::Arc,
+    time::Duration,
+};
 
-use axum::Router;
+use anyhow::Ok;
 use sqlx::postgres::PgPoolOptions;
+use tower_governor::{GovernorLayer, governor::GovernorConfigBuilder};
+use utoipa::OpenApi;
+use utoipa_axum::router::OpenApiRouter;
+use utoipa_swagger_ui::SwaggerUi;
 
 use crate::{
-    application::services::auth_service::AuthService,
+    application::auth::service::AuthService,
     config::Config,
-    infrastructure::persistence::{pg_auth_repo::PgAuthRepo, pg_user_repo::PgUserRepo},
-    presentation::handlers::auth::create_auth_router,
+    infrastructure::{
+        auth::{argon2_hasher::Argon2Hasher, jwt_token_provider::JwtTokenProvider},
+        persistence::{
+            pg_authentication_repository::PgAuthenticationRepository,
+            pg_user_repository::PgUserRepository,
+        },
+    },
+    presentation::{cookie::CookieConfig, handlers::auth::create_auth_router},
 };
 
 pub mod config;
-pub mod error;
 
 pub mod application;
 pub mod domain;
 pub mod infrastructure;
 pub mod presentation;
 
+#[derive(OpenApi)]
+#[openapi()]
+struct ApiDoc;
+
+type ConcreteAuthService =
+    AuthService<PgAuthenticationRepository, PgUserRepository, Argon2Hasher, JwtTokenProvider>;
+
 #[derive(Clone)]
 pub struct AppState {
-    auth_service: Arc<AuthService<PgAuthRepo, PgUserRepo>>,
+    auth_service: Arc<ConcreteAuthService>,
+    cookie_config: Arc<CookieConfig>,
 }
 
 #[tokio::main]
@@ -32,24 +53,60 @@ async fn main() -> anyhow::Result<()> {
         )
         .init();
 
+    let governor_conf = GovernorConfigBuilder::default()
+        .per_second(10)
+        .burst_size(30)
+        .finish()
+        .expect("failed to build governor config");
+    let governor_limiter = governor_conf.limiter().clone();
+    let interval = Duration::from_secs(60);
+    tokio::spawn(async move {
+        loop {
+            tokio::time::sleep(interval).await;
+            tracing::info!("rate limiting storage size: {}", governor_limiter.len());
+            governor_limiter.retain_recent();
+        }
+    });
+
     let config = Config::from_env()?;
     let pool = PgPoolOptions::new()
         .max_connections(5)
         .connect(&config.database_url)
         .await?;
 
-    let user_repo = PgUserRepo::new(pool.clone());
-    let auth_repo = PgAuthRepo::new(pool.clone());
-    let auth_service = Arc::new(AuthService::new(auth_repo, user_repo));
+    let cookie_config = Arc::new(CookieConfig::from_env(config.app_env));
 
-    let app_state = AppState { auth_service };
+    let user_repo = PgUserRepository::new(pool.clone());
+    let auth_repo = PgAuthenticationRepository::new(pool.clone());
+    let token_provider = JwtTokenProvider::new(config.jwt_secret, config.jwt_expiration);
+    let auth_service = Arc::new(AuthService::new(
+        auth_repo,
+        user_repo,
+        Argon2Hasher,
+        token_provider,
+    ));
 
-    let auth_router = create_auth_router();
-    let router = Router::new().merge(auth_router).with_state(app_state);
+    let app_state = AppState {
+        auth_service,
+        cookie_config,
+    };
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+    let (router, api) = OpenApiRouter::with_openapi(ApiDoc::openapi())
+        .nest("/auth", create_auth_router())
+        .split_for_parts();
+    let router = router
+        .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", api))
+        .layer(GovernorLayer::new(governor_conf))
+        .with_state(app_state);
+
+    let address = SocketAddr::from((Ipv4Addr::UNSPECIFIED, 3000));
+    let listener = tokio::net::TcpListener::bind(&address).await?;
     tracing::debug!("listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, router).await?;
+    axum::serve(
+        listener,
+        router.into_make_service_with_connect_info::<std::net::SocketAddr>(),
+    )
+    .await?;
 
     Ok(())
 }
