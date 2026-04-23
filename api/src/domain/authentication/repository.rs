6e8d4dfb079fca974f8dccddc4a32e57c1c08c0e
nagokos <kosnago0428@gmail.com>
2026-04-23 11:39:25 +0900

    fix(api): Fix for Google login bug

--- api/src/domain/authentication/repository.rs
+++ api/src/domain/authentication/repository.rs
@@ -3,13 +3,14 @@ use async_trait::async_trait;
 use crate::domain::{
     authentication::model::{Authentication, AuthenticationPayload, Provider},
     id::Id,
-    user::model::User,
+    user::model::{User, UserPayload},
 };
 
 #[async_trait]
 pub trait AuthRepository: Send + Sync {
     async fn create_user_with_authentication(
         &self,
+        user: UserPayload,
         authentication_provider: AuthenticationPayload,
     ) -> Result<User, anyhow::Error>;
     async fn update_user_with_authentication(
