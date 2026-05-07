e631e72e2b047f3a3024500e2500e0d452b7f86a
nagokos <kosnago0428@gmail.com>
2026-05-07 16:02:14 +0900

    refactor(db): Renamed table from release_group_recordings to tracks

--- api/migrations/20260524161327_create_tracks.up.sql
+++ api/migrations/20260524161327_create_tracks.up.sql
@@ -1,16 +1,17 @@
-CREATE TABLE release_group_recordings (
+CREATE TABLE tracks (
   id                BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   release_group_id  BIGINT NOT NULL REFERENCES release_groups(id) ON DELETE CASCADE,
   recording_id      BIGINT NOT NULL REFERENCES recordings(id) ON DELETE CASCADE,
-  track_mbid        UUID UNIQUE,
+  mbid              UUID UNIQUE,
   disc_number       SMALLINT NOT NULL CHECK (disc_number > 0),
   track_number      SMALLINT NOT NULL CHECK (track_number > 0),
   created_at        TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   updated_at        TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+
   UNIQUE (release_group_id, recording_id),
   UNIQUE (release_group_id, disc_number, track_number)
 );
 
 CREATE TRIGGER set_updated_at
-    BEFORE UPDATE ON release_group_recordings
+    BEFORE UPDATE ON tracks
     FOR EACH ROW EXECUTE FUNCTION update_updated_at();
